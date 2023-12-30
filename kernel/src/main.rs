73cf55dc8c75a06772be8add6dc913875090da02
debater-coder <52619668+debater-coder@users.noreply.github.com>
2023-12-30 14:51:30 +1100

    feat: byeee triple faults

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,12 +2,12 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
 use crate::gop_buffer::Writer;
+use core::panic::PanicInfo;
 
-
-mod gop_buffer;
 mod debug_log;
+mod gdt;
+mod gop_buffer;
 mod interrupts;
 
 /// This function is called on panic.
@@ -17,19 +17,35 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+bootloader_api::entry_point!(kernel_early);
 
-bootloader_api::entry_point!(kernel_main);
+fn kernel_early(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+    let framebuffer = boot_info
+        .framebuffer
+        .as_mut()
+        .expect("Expected framebuffer");
 
-fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
-    let framebuffer = boot_info.framebuffer.as_mut().expect("Expected framebuffer");
-    let (framebuffer_info, raw_framebuffer) = (framebuffer.info().clone(), framebuffer.buffer_mut());
+    let (framebuffer_info, raw_framebuffer) =
+        (framebuffer.info().clone(), framebuffer.buffer_mut());
 
     unsafe { Writer::init(raw_framebuffer, framebuffer_info) };
+
+    gdt::init();
     interrupts::init_idt();
 
+    main();
+
+    loop {
+        x86_64::instructions::hlt();
+    } // main should never return but if it does we halt
+}
+
+fn main() {
     println!("Hello, World!");
 
-    x86_64::instructions::interrupts::int3();
+    fn overflow() {
+        overflow();
+    }
 
-    loop {}
+    overflow();
 }
